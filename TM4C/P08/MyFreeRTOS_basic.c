959e1e46b195ba4f4737fc90c9e5fd360d71c6c9
Jose Leitao <jose.p.leitao@gmail.com>
2018-11-11 17:28:36 -0200

    Criação do branch P08

---
+++ TM4C/P08/MyFreeRTOS_basic.c
@@ -0,0 +1,33 @@
+/* *******************************************
+ * Etapa 8 - instalação dor FreeRTOS na sua
+ * configuração mais simples (2 tarefas)
+ *
+ *
+ ********************************************/
+
+
+
+#include <stdint.h>
+#include <FreeRTOS.h>
+#include <task.h>
+
+void vPeriodicTask(void *pvParameters){
+// Establish the task's period.
+    const TickType_t xDelay = pdMS_TO_TICKS(1000);
+    TickType_t xLastWakeTime = xTaskGetTickCount();
+    for (;;) {
+// Block until the next release time.
+             vTaskDelayUntil(&xLastWakeTime, xDelay);
+    }
+}
+/* Global variables */
+uint32_t SystemCoreClock = 16000000;
+
+int main(void){
+xTaskCreate(vPeriodicTask, "My Task", 256, NULL, 1, NULL);
+// Startup of the FreeRTOS scheduler. The program should block here.
+vTaskStartScheduler();
+// The following line should never be reached. Failure to allocate enough
+// memory from the heap would be one reason.
+for (;;);
+}
