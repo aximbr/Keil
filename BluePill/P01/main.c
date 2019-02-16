c60059820b5750bb14d54416366f29831ac516b0
Jose Leitao <jose.p.leitao@gmail.com>
2019-02-16 13:05:43 -0200

    Atualização P14 do BluePill e Criado na pasta para ST32F030
    Signed-off-by: Jose Leitao <jose.p.leitao@gmail.com>

--- BluePill/P01/main.c
+++ BluePill/P01/main.c
@@ -1,5 +1,5 @@
 /*Etapa 1 - Acender o Led user1 em PC13 */
-/* Alterada para testar a PA.4 */
+/* Alterada para testar a PC.13 */
 
 /*MCU STM32F103C8 */
 #include "stm32f10x.h"
@@ -11,16 +11,16 @@ void delay(void){
 
 int main(void){
 /*inicializa a placa */
-	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOA, ENABLE);                //1) Habilita o clock da GPIOA
-	GPIO_PinConfigure(GPIOA, 4, GPIO_OUT_PUSH_PULL, GPIO_MODE_OUT2MHZ);  //2) Configura para output vel max de 2Mhz, push-pull            
+	RCC_APB2PeriphClockCmd( RCC_APB2Periph_GPIOC, ENABLE);                //1) Habilita o clock da GPIOC
+	GPIO_PinConfigure(GPIOC, 13, GPIO_OUT_PUSH_PULL, GPIO_MODE_OUT2MHZ);  //2) Configura para output vel max de 2Mhz, push-pull            
 	       
 	
 			
 	while(1) //Loop infinito
 	{ 
-		GPIO_PinWrite(GPIOA, 4, 0);
+		GPIO_PinWrite(GPIOC, 13, 0);
 		delay();
-		GPIO_PinWrite(GPIOA, 4, 1);
+		GPIO_PinWrite(GPIOC, 13, 1);
 		delay();
 	}
 }
